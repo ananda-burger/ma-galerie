0938db3e6450b848022d89652462fc012e95bc8f
aburger <aburger.ananda@gmail.com>
2021-07-04 18:33:54 -0300

    Change initial PICTURES

--- src/App.js
+++ src/App.js
@@ -15,31 +15,35 @@ const PICTURES = [
     id: 1,
     image: 'https://wallpapercave.com/wp/wp4302820.jpg',
     title: 'A nice pink background',
-    likes: false
+    likes: false,
+    boards: ['pink']
   },
   {
     id: 2,
     image: 'https://wallpaperaccess.com/full/1138983.jpg',
     title: 'A tron inspired image',
-    likes: false
+    likes: false,
+    boards: ['black']
   },
   {
     id: 3,
     image:
       'https://uhdwallpapers.org/uploads/converted/19/08/03/huawei-mediapad-m6-stock-wallpaper-1024x1024_666874-mm-90.jpg',
     title: 'Another tron inspired image',
-    likes: true
+    likes: false,
+    boards: ['pink', 'blue']
   },
   {
     id: 4,
     image: 'https://cdn.wallpapersafari.com/21/87/qjJXvV.jpg',
     title: 'A nice geometric image',
-    likes: false
+    likes: false,
+    boards: ['pink', 'black', 'blue']
   }
 ]
 
 const App = () => {
-  const [selectedFilter, setSelectedFilter] = useState('liked')
+  const [selectedFilter, setSelectedFilter] = useState('all')
   const [images, setImages] = useState(PICTURES)
 
   return (
