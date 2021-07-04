582f4ec6f95c4338aa18785627e8b5cf66658685
aburger <aburger.ananda@gmail.com>
2021-07-04 10:29:15 -0300

    Create heart button for likes

--- src/components/images/ImagesList.js
+++ src/components/images/ImagesList.js
@@ -1,19 +1,33 @@
-// import ImageItem from './ImageItem.js'
+import { useState } from 'react'
+
+import FullHeart from '../icons/FullHeart.js'
+import HollowHeart from '../icons/HollowHeart.js'
 import classes from './ImagesList.module.css'
 
 const ImagesList = (props) => {
+  const [renderImages, setRenderImages] = useState(props.images)
+
+  const hasLikes = (id) => {
+    const index = renderImages.findIndex((image) => image.id === id)
+    renderImages[index].likes = !renderImages[index].likes
+    const updatedImages = [...renderImages]
+
+    setRenderImages(updatedImages)
+  }
+
   return (
     <ul className={classes.list}>
-      {props.images.map((img) => {
+      {renderImages.map((img) => {
         return (
           <li key={img.id} className={classes.item}>
             <img src={img.image} alt={img.title} className={classes.image} />
             <div className={classes.text}>{img.title}</div>
-            <button
-              onClick={() => console.log('cliquei!')}
-              className={classes.likes}
-            >
-              ♡
+            <button onClick={() => hasLikes(img.id)} className={classes.likes}>
+              {img.likes ? (
+                <FullHeart className="icon-small" />
+              ) : (
+                <HollowHeart className="icon-small" />
+              )}
             </button>
           </li>
         )
--- src/pages/AllPictures.js
+++ src/pages/AllPictures.js
@@ -4,23 +4,27 @@ const PICTURES = [
   {
     id: 1,
     image: 'https://wallpapercave.com/wp/wp4302820.jpg',
-    title: 'A nice pink background'
+    title: 'A nice pink background',
+    likes: false
   },
   {
     id: 2,
     image: 'https://wallpaperaccess.com/full/1138983.jpg',
-    title: 'A tron inspired image'
+    title: 'A tron inspired image',
+    likes: false
   },
   {
     id: 3,
     image:
       'https://uhdwallpapers.org/uploads/converted/19/08/03/huawei-mediapad-m6-stock-wallpaper-1024x1024_666874-mm-90.jpg',
-    title: 'Another tron inspired image'
+    title: 'Another tron inspired image',
+    likes: false
   },
   {
     id: 4,
     image: 'https://cdn.wallpapersafari.com/21/87/qjJXvV.jpg',
-    title: 'A nice geometric image'
+    title: 'A nice geometric image',
+    likes: false
   }
 ]
 
