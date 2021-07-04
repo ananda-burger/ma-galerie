c70f54ca70ee721f11a580912932dfd48313f01c
aburger <aburger.ananda@gmail.com>
2021-07-04 16:32:07 -0300

    Move ImagesList to parent component

--- src/pages/AllPictures.js
+++ src/pages/AllPictures.js
@@ -1,7 +1,49 @@
-import ImagesList from '../components/images/ImagesList.js'
+import FullHeart from '../components/icons/FullHeart.js'
+import HollowHeart from '../components/icons/HollowHeart.js'
+import classes from './AllPictures.module.css'
 
 const AllPicturesPage = (props) => {
-  return <ImagesList {...props} />
+  let filteredImages = props.images
+  if (props.selectedFilter !== 'all') {
+    filteredImages = filteredImages.filter((image) => {
+      if (props.selectedFilter === 'liked') {
+        return image.likes === true
+      } else {
+        return image.likes === false
+      }
+    })
+  }
+
+  const toggleLike = (id) => {
+    const index = props.images.findIndex((image) => image.id === id)
+    props.images[index].likes = !props.images[index].likes
+    const updatedImages = [...props.images]
+
+    props.setImages(updatedImages)
+  }
+
+  return (
+    <ul className={classes.list}>
+      {filteredImages.map((img) => {
+        return (
+          <li key={img.id} className={classes.item}>
+            <img src={img.image} alt={img.title} className={classes.image} />
+            <div className={classes.text}>{img.title}</div>
+            <button
+              onClick={() => toggleLike(img.id)}
+              className={classes.likes}
+            >
+              {img.likes ? (
+                <FullHeart className="icon-small" />
+              ) : (
+                <HollowHeart className="icon-small" />
+              )}
+            </button>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
 
 export default AllPicturesPage
