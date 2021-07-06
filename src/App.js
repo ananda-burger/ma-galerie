a1c18bf18410378ea2b14df87dae7c611a56bd9e
aburger <aburger.ananda@gmail.com>
2021-07-06 15:40:44 -0300

    Add more images to gallery

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ const PICTURES = [
   {
     id: 1,
     image: 'https://wallpapercave.com/wp/wp4302820.jpg',
-    title: 'A nice pink background',
+    title: 'A nice pink square',
     likes: false,
     boards: ['pink']
   },
   {
     id: 2,
     image: 'https://wallpaperaccess.com/full/1138983.jpg',
-    title: 'A tron inspired image',
+    title: 'Psychodelic future',
     likes: false,
     boards: ['black']
   },
@@ -29,16 +29,64 @@ const PICTURES = [
     id: 3,
     image:
       'https://uhdwallpapers.org/uploads/converted/19/08/03/huawei-mediapad-m6-stock-wallpaper-1024x1024_666874-mm-90.jpg',
-    title: 'Another tron inspired image',
+    title: 'I love TRON',
     likes: false,
     boards: ['pink', 'blue']
   },
   {
     id: 4,
     image: 'https://cdn.wallpapersafari.com/21/87/qjJXvV.jpg',
-    title: 'A nice geometric image',
+    title: 'Space dude',
     likes: false,
     boards: ['pink', 'black', 'blue']
+  },
+  {
+    id: 5,
+    image:
+      'https://hdwallpaperim.com/wp-content/uploads/2017/08/25/462707-universe-galaxy-space-stars-748x748.jpg',
+    title: 'Deep Space',
+    likes: false,
+    boards: ['black', 'blue']
+  },
+  {
+    id: 6,
+    image:
+      'https://9to5mac.com/wp-content/uploads/sites/6/2020/10/1761.Desert_Tree_Day-375w-812h@3xiphone.png',
+    title: 'One single tree',
+    likes: false,
+    boards: ['blue']
+  },
+  {
+    id: 7,
+    image:
+      'https://9to5mac.com/wp-content/uploads/sites/6/2020/10/1791.Lake_The_Cliff_Day-375w-812h@3xiphone.png',
+    title: 'A ride on the mountain',
+    likes: false,
+    boards: ['pink', 'blue', 'black']
+  },
+  {
+    id: 8,
+    image:
+      'https://9to5mac.com/wp-content/uploads/sites/6/2020/10/1751.Desert_Peak_Day-375w-812h@3xiphone.png?resize=2048,2048',
+    title: 'Nice calm mountain',
+    likes: false,
+    boards: ['blue']
+  },
+  {
+    id: 9,
+    image:
+      'https://9to5mac.com/wp-content/uploads/sites/6/2020/10/1811.Lake_The_Desert_Day-375w-812h@3xiphone.png',
+    title: 'Nice desert day',
+    likes: false,
+    boards: ['pink', 'blue']
+  },
+  {
+    id: 10,
+    image:
+      'https://9to5mac.com/wp-content/uploads/sites/6/2020/10/1821.Lake_The_Beach_Day-375w-812h@3xiphone.png',
+    title: 'Lost on shore',
+    likes: false,
+    boards: ['black', 'blue']
   }
 ]
 
@@ -46,22 +94,29 @@ const App = () => {
   const [selectedFilter, setSelectedFilter] = useState('all')
   const [selectedBoardFilter, setSelectedBoardFilter] = useState([])
   const [images, setImages] = useState(PICTURES)
+  const [imagesDisplayed, setImagesDisplayed] = useState(2)
 
   return (
     <div>
-      <Intro />
+      {/* <Intro /> */}
       <MainNavigation
         selectedFilter={selectedFilter}
         setSelectedFilter={setSelectedFilter}
         selectedBoardFilter={selectedBoardFilter}
         setSelectedBoardFilter={setSelectedBoardFilter}
+        imagesDisplayed={imagesDisplayed}
+        setImagesDisplayed={setImagesDisplayed}
       />
       <Route path="/" exact>
         <AllPicturesPage
           selectedFilter={selectedFilter}
           setSelectedFilter={setSelectedFilter}
-          setImages={setImages}
+          selectedBoardFilter={selectedBoardFilter}
+          setSelectedBoardFilter={setSelectedBoardFilter}
           images={images}
+          setImages={setImages}
+          imagesDisplayed={imagesDisplayed}
+          setImagesDisplayed={setImagesDisplayed}
         />
       </Route>
       <Route path="/favorites">
