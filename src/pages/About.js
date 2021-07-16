b22cdd2500379666c87d2b472c1bf240999b4af4
aburger <aburger.ananda@gmail.com>
2021-07-16 20:29:50 -0300

    Restructuring contact and about pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,27 @@
+import classes from './About.module.css'
+
 const AboutPage = () => {
   return (
-    <div>
-      <div>About Page</div>
+    <div className={classes.flex}>
+      <div className={classes.info}>
+        <p className={classes.text}>Hello!</p>
+        <p className={classes.text}>
+          My name is Ananda Burger and I am a software developer who loves to
+          draw! I make traditional art as well as digital art and focus on
+          lanscape and figure drawing. Since young I have loved animes and
+          japanese art, it definetly has modelled my art style.
+        </p>
+        <p className={classes.text}>
+          I sell my art and make custom illustrations of various matters and if
+          you want to talk about a custom order or discuss any idea you may have
+          it will be super awesome talking to you!
+        </p>
+      </div>
+      <img
+        src="https://live.staticflickr.com/65535/51315952961_f63a78065e_k.jpg"
+        alt="Self Portrait"
+        className={classes.portrait}
+      />
     </div>
   )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,33 +2,41 @@ import classes from './Contact.module.css'
 import LinkedIn from '../components/icons/LinkedIn.js'
 import Facebook from '../components/icons/Facebook.js'
 import Instagram from '../components/icons/Instagram.js'
+import Email from '../components/icons/Email.js'
 
 const ContactPage = () => {
   return (
     <div>
-      <div>
-        <h2 className={classes.call}>Get in touch to know more about me</h2>
-        <ul className={classes.list}>
-          <li className={classes.item}>
-            <Instagram />
-            <a href="https://www.instagram.com/" className={classes.black}>
-              Instagram
-            </a>
-          </li>
-          <li className={classes.item}>
-            <LinkedIn />
-            <a href="https://www.linkedin.com/" className={classes.black}>
-              LinkedIn
-            </a>
-          </li>
-          <li className={classes.item}>
-            <Facebook />
-            <a href="https://www.facebook.com/" className={classes.black}>
-              Facebook
-            </a>
-          </li>
-        </ul>
-      </div>
+      <h2 className={classes.call}>Get in touch to know more about me :)</h2>
+      <ul className={classes.list}>
+        <li className={classes.item}>
+          <Email />
+          <p className={classes.text}>aburger.ananda@gmail.com</p>
+        </li>
+        <li className={classes.item}>
+          <Instagram />
+          <a href="https://www.instagram.com/" className={classes.text}>
+            Instagram
+          </a>
+        </li>
+        <li className={classes.item}>
+          <LinkedIn />
+          <a href="https://www.linkedin.com/" className={classes.text}>
+            LinkedIn
+          </a>
+        </li>
+        <li className={classes.item}>
+          <Facebook />
+          <a href="https://www.facebook.com/" className={classes.text}>
+            Facebook
+          </a>
+        </li>
+      </ul>
+      <img
+        src="https://live.staticflickr.com/65535/51315197802_831b74ee0b_c.jpg"
+        alt="Self Portrait"
+        className={classes.portrait}
+      />
     </div>
   )
 }
