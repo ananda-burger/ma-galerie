a9e9f1fdc76797fa295cf1a94a0782e7550e8e3d
aburger <aburger.ananda@gmail.com>
2021-07-21 14:31:49 -0300

    Deals with exception of image id not found

--- src/domain/image.js
+++ src/domain/image.js
@@ -5,6 +5,9 @@ const tagFilter = (tag) => (image) => {
 const toggleLike = (images, id) => {
   const updatedImages = [...images]
   const index = updatedImages.findIndex((image) => image.id === id)
+
+  if (index === -1) throw new Error('Image id was not found')
+
   updatedImages[index] = {
     ...updatedImages[index],
     likes: !updatedImages[index].likes
