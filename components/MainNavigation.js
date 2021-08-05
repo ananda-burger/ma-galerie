b841b15ab2c51a4615b5aaf90f5fc01ed3e8b28d
aburger <aburger.ananda@gmail.com>
2021-08-05 15:42:56 -0300

    Improve main navigation design

--- components/MainNavigation.js
+++ components/MainNavigation.js
@@ -1,25 +1,42 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import classes from './MainNavigation.module.css'
 
 export default function MainNavigation() {
+  const router = useRouter()
+
   return (
     <header className={classes.head}>
       <div className={classes.title}>
-        <Link  href="/">
-          <a className={classes.transition}>
-          Ma Galerie
+        <Link href="/">
+          <a
+            className={
+              router.pathname === '/'
+                ? classes.selectedTitle
+                : classes.notSelected
+            }
+          >
+            Ma Galerie
           </a>
         </Link>
       </div>
       <Link href="/about">
-        <a className={classes.nav}>
-        About
+        <a
+          className={
+            router.pathname === '/about' ? classes.selectedNav : classes.nav
+          }
+        >
+          About
         </a>
       </Link>
       <Link href="/contact">
-        <a className={classes.nav}>
-        Contact
+        <a
+          className={
+            router.pathname === '/contact' ? classes.selectedNav : classes.nav
+          }
+        >
+          Contact
         </a>
       </Link>
     </header>
